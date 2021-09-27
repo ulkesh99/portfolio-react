ed73c0cb8efdd4798055bd784f51795cd64934aa
Ulkesh Chendwankar <ulkeshchendwankar1@gmail.com>
2021-09-28 00:57:36 +0530

    Updates Skills.js
    aligned items

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -7,32 +7,32 @@ const Skills = () => {
             <div className="container-fluid">
                 <div className="lang">
                  <div className="row">
-                        <div className="lang col-md-3 col-sm-6">
+                        <div className="col-md-3 col-sm-6">
                             <div className="box">
-                            <img class="skills-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                                <h3>JavaScript</h3>
-                            </div>
+                            <img class="skills-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" /><br />
+                                <h3 class="skills-title">JavaScript</h3>
+                            </div><br /><br /><hr />
                         </div>
 
-                        <div className="lang col-md-3 col-sm-6">
+                        <div className="col-md-3 col-sm-6">
                             <div className="box">
-                            <img class="skills-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-                                <h3>React</h3>
-                            </div>
+                            <img class="skills-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" /><br />
+                                <h3 class="skills-title">React</h3>
+                            </div><br /><br /><hr />
                         </div>
 
-                        <div className="lang col-md-3 col-sm-6">
-                            <div className="box"><img class="skills-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" />
-                                <h3>MySQL</h3>
-                            </div>
+                        <div className="col-md-3 col-sm-6">
+                            <div className="box"><img class="skills-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" /><br />
+                                <h3 class="skills-title">MySQL</h3>
+                            </div><br /><br /><hr />
                         </div>
 
-                        <div className="lang col-md-3 col-sm-6">
+                        <div className="col-md-3 col-sm-6">
                             <div className="box">
                             <img class="skills-logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
 
-                                <h3>Figma</h3>
-                            </div>
+                                <h3 class="skills-title">Figma</h3>
+                            </div><br /><br /><hr />
                         </div>
                     </div>
                     </div>
